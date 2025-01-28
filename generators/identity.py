322add1d4ac35d64976037ee5a207e811d67443c
David G <himynamesdave@users.noreply.github.com>
2025-01-28 06:43:10 +0000

    adding r2s

--- generators/identity.py
+++ generators/identity.py
@@ -34,8 +34,8 @@ identity_names = [
     "arango_cti_processor", "cve2stix", "cwe2stix", "disarm2stix",
     "sigma2stix", "dogesec", "stix2arango", "txt2stix",
     "yara2stix", "location2stix", "creditcard2stix",
-    "feeds2stix", "ransomwarekb", "stixify", "obstracts", "siemrules",
-    "arango_cve_processor", "txt2detection"
+    "feeds2stix", "stixify", "obstracts", "siemrules",
+    "arango_cve_processor", "txt2detection", "ransomware2stix"
 ]
 
 # Ensure the output directory for individual identity objects exists
