b32823eed80049b40ec7062427ea7e9f43fc08a5
David G <himynamesdave@users.noreply.github.com>
2026-07-20 15:34:46 +0100

    remove cloak2stix identity

--- generators/identity.py
+++ generators/identity.py
@@ -33,7 +33,7 @@ identities = []
 identity_names = [
     "sigma2stix", "dogesec", "stix2arango", "txt2stix",
     "yara2stix", "feeds2stix", "stixify", "obstracts", "siemrules",
-    "txt2detection", "ransomware2stix", "cloak2stix"
+    "txt2detection", "ransomware2stix"
 ]
 
 # Ensure the output directory for individual identity objects exists
